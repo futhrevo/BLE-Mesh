f99cd0ae112ba8446be5cc44b559f59e10a81a5d
Rakesh Kalyankar <k.rakeshlal@gmail.com>
2018-10-16 17:26:09 +0530

    scan timeout reading from settings
    Took 1 hour 18 minutes

--- app/src/main/java/in/hedera/reku/swimclock/settings/SettingsFragment.java
+++ app/src/main/java/in/hedera/reku/swimclock/settings/SettingsFragment.java
@@ -9,6 +9,8 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.SeekBarPreference;
+import android.util.Log;
 
 import java.util.Objects;
 
@@ -26,17 +28,34 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.app_preferences);
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
     }
 
     @Override
-    public void onCreatePreferences(Bundle bundle, String s) {
-
+    public void onCreatePreferences(Bundle bundle, String rootKey) {
+        addPreferencesFromResource(R.xml.app_preferences);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SeekBarPreference seekBar = (SeekBarPreference) findPreference(getString(R.string.pref_scan_timeout));
+        if(seekBar != null) {
+            seekBar.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object o) {
+                    if(o instanceof Integer) {
+                        Log.d(TAG, "value is " + String.valueOf(o));
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt(getString(R.string.pref_scan_timeout), (Integer) o);
+                        editor.apply();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+        }
     }
 
     @Override
     public boolean onPreferenceTreeClick(Preference preference) {
+        Log.d(TAG, preference.getKey());
         if(preference.getKey().equals(getString(R.string.pref_delete_network))) {
             AlertDialog.Builder builder = new AlertDialog.Builder(Objects.requireNonNull(getActivity()));
             builder.setMessage("Are you sure ? ").setTitle("Delete Network");
