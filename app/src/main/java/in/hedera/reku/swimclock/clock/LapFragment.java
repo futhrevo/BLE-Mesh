9754197ffc192b33158ec5491e5d097ba75cdf46
Rakesh Kalyankar <k.rakeshlal@gmail.com>
2018-12-14 21:05:18 +0530

    single lap working
    Took 59 minutes

--- app/src/main/java/in/hedera/reku/swimclock/clock/LapFragment.java
+++ app/src/main/java/in/hedera/reku/swimclock/clock/LapFragment.java
@@ -3,6 +3,7 @@ package in.hedera.reku.swimclock.clock;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -13,12 +14,12 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import in.hedera.reku.swimclock.FragListener;
 import in.hedera.reku.swimclock.MainActivity;
 import in.hedera.reku.swimclock.R;
 import in.hedera.reku.swimclock.utils.BtnState;
-import in.hedera.reku.swimclock.utils.Chronometer;
 import in.hedera.reku.swimclock.utils.Constants;
 import in.hedera.reku.swimclock.utils.LapSet;
 
@@ -38,11 +39,16 @@ public class LapFragment extends Fragment {
     private EditText etIntMin;
     private EditText etIntSec;
     private EditText etIntms;
+    private TextView timeview;
     private int laps;
     private int currentlap;
     private long timeWhenPaused = 0;
 
-    private Chronometer chronometer;
+    long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L;
+
+    Handler handler;
+
+//    private Chronometer chronometer;
 
     public LapFragment() {
         // Required empty public constructor
@@ -63,6 +69,7 @@ public class LapFragment extends Fragment {
                              Bundle savedInstanceState) {
         callback.sendOpcode("02");
         lapSet = new LapSet();
+        handler = new Handler() ;
         View rootView = inflater.inflate(R.layout.fragment_lap, container, false);
         Button resetBtn = rootView.findViewById(R.id.reset_btn);
         resetBtn.setOnClickListener(new View.OnClickListener() {
@@ -83,7 +90,7 @@ public class LapFragment extends Fragment {
         lapDirection.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(((CheckBox) v).isChecked()) {
+                if (((CheckBox) v).isChecked()) {
                     ld = "01";
                 } else {
                     ld = "00";
@@ -94,7 +101,7 @@ public class LapFragment extends Fragment {
         actionBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Button b = (Button)v;
+                Button b = (Button) v;
                 String label = b.getText().toString();
                 switch (label) {
                     case Constants.BTN_START:
@@ -124,18 +131,19 @@ public class LapFragment extends Fragment {
                 etIntms.setText("");
             }
         });
-        chronometer = rootView.findViewById(R.id.timeview);
-        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
-            @Override
-            public void onChronometerTick(Chronometer chronometer) {
-                if(chronometer.getTimeElapsed() > lapSet.getTotalMills()) {
-                    chronometer.stop();
-                    // make this as runnable with delay
-                    restartCounter();
-
-                }
-            }
-        });
+        timeview = rootView.findViewById(R.id.timeview);
+//        chronometer = rootView.findViewById(R.id.timeview);
+//        chronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
+//            @Override
+//            public void onChronometerTick(Chronometer chronometer) {
+//                if (chronometer.getTimeElapsed() > lapSet.getTotalMills()) {
+//                    chronometer.stop();
+//                    // make this as runnable with delay
+//                    restartCounter();
+//
+//                }
+//            }
+//        });
         return rootView;
     }
 
@@ -147,45 +155,126 @@ public class LapFragment extends Fragment {
         callback.sendOpcode("04" + ld + lapSet.getLapcount() + lapSet.getMillisHex());
         laps = lapSet.getlapcountInt();
         currentlap = 0;
-        chronometer.setBase(SystemClock.elapsedRealtime());
-        chronometer.start();
+//        chronometer.setBase(SystemClock.elapsedRealtime());
+//        chronometer.start();
+        if(ld.equals("00")) {
+            StartTime = SystemClock.uptimeMillis() + lapSet.getTotalMills();
+            handler.postDelayed(downRunnable, 0);
+        } else {
+            StartTime = SystemClock.uptimeMillis();
+            handler.postDelayed(uprunnable, 0);
+        }
         actionBtn.setText(Constants.BTN_PAUSE);
     }
 
     private void pauseLaps() {
         callback.sendOpcode("0501");
-        timeWhenPaused = chronometer.getBase() - SystemClock.elapsedRealtime();
-        chronometer.stop();
+//        timeWhenPaused = chronometer.getBase() - SystemClock.elapsedRealtime();
+//        chronometer.stop();
+        TimeBuff += MillisecondTime;
+
+        handler.removeCallbacks(uprunnable);
+
         actionBtn.setText(Constants.BTN_RESUME);
     }
 
     private void resumeLaps() {
         callback.sendOpcode("0500");
-        chronometer.setBase(SystemClock.elapsedRealtime() + timeWhenPaused);
-        chronometer.start();
+//        chronometer.setBase(SystemClock.elapsedRealtime() + timeWhenPaused);
+//        chronometer.start();
+        StartTime = SystemClock.uptimeMillis();
+        handler.postDelayed(uprunnable, 0);
         actionBtn.setText(Constants.BTN_PAUSE);
     }
 
     private void resetLaps() {
         callback.sendOpcode("06");
         actionBtn.setText(Constants.BTN_START);
-        chronometer.stop();
-        chronometer.setBase(SystemClock.elapsedRealtime());
+//        chronometer.stop();
+//        chronometer.setBase(SystemClock.elapsedRealtime());
+        handler.removeCallbacks(uprunnable);
+    }
+
+    private void resetRunnable() {
+        MillisecondTime = 0L ;
+        StartTime = 0L ;
+        TimeBuff = 0L ;
+        UpdateTime = 0L ;
+        timeview.setText("00:00.00");
     }
 
     public void restartCounter() {
-            currentlap++;
-            Log.d(TAG, String.valueOf(currentlap));
-            if(laps >= currentlap) {
-                chronometer.setBase(SystemClock.elapsedRealtime());
-                chronometer.start();
+        currentlap++;
+        Log.d(TAG, String.valueOf(currentlap));
+        if (laps >= currentlap) {
+//            chronometer.setBase(SystemClock.elapsedRealtime());
+//            chronometer.start();
+        }
+    }
+
+    public void updateTimeView(long millis) {
+        int Seconds = (int) (millis / 1000);
+
+        int Minutes = Seconds / 60;
+
+        Seconds = Seconds % 60;
+
+        int MilliSeconds = (int) (millis % 1000);
+
+        timeview.setText("" + Minutes + ":"
+                + String.format("%02d", Seconds) + ":"
+                + String.format("%03d", MilliSeconds));
+    }
+
+    public Runnable uprunnable = new Runnable() {
+        @Override
+        public void run() {
+            MillisecondTime = SystemClock.uptimeMillis() - StartTime;
+
+            UpdateTime = TimeBuff + MillisecondTime;
+
+            updateTimeView(UpdateTime);
+
+            if (MillisecondTime > lapSet.getTotalMills()) {
+                currentlap++;
+                Log.d(TAG, String.valueOf(currentlap) + " <- " + String.valueOf(laps));
+                resetRunnable();
+                StartTime = SystemClock.uptimeMillis();
+                if (laps > currentlap) {
+                    handler.postDelayed(this, 0);
+                } else {
+                    handler.removeCallbacks(uprunnable);
+                    currentlap = 0;
+                    actionBtn.setText(Constants.BTN_START);
+                }
+            }else {
+                handler.postDelayed(this, 0);
+            }
+
+        }
+    };
+
+    public Runnable downRunnable = new Runnable() {
+        @Override
+        public void run() {
+            MillisecondTime = StartTime - SystemClock.uptimeMillis();
+            UpdateTime = TimeBuff + MillisecondTime;
+            updateTimeView(UpdateTime);
+            if(MillisecondTime < 0) {
+                currentlap++;
+                resetRunnable();
+                StartTime = SystemClock.uptimeMillis() + lapSet.getTotalMills();
+                Log.d(TAG, String.valueOf(currentlap) + " <- " + String.valueOf(laps));
+                if (laps > currentlap) {
+                    handler.postDelayed(this, 0);
+                } else {
+                    handler.removeCallbacks(uprunnable);
+                    currentlap = 0;
+                    actionBtn.setText(Constants.BTN_START);
+                }
+            } else {
+                handler.postDelayed(this, 0);
             }
         }
-
-        public Runnable uprunnable = new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        };
+    };
 }
